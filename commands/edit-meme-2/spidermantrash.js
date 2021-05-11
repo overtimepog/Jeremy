5af35d5ab4a5ecfe7d2307a69264d218a9957af2
Overtime <Truen2005@gmail.com>
2021-05-11 12:43:59 -0400

    added trash

--- commands/edit-meme-2/spidermantrash.js
+++ commands/edit-meme-2/spidermantrash.js
@@ -2,14 +2,14 @@ const Command = require('../../structures/Command');
 const DIG = require("discord-image-generation");
 const Discord = require("discord.js")
 
-module.exports = class DeleteCommand extends Command {
+module.exports = class TrashCommand extends Command {
 	constructor(client) {
 		super(client, {
-			name: 'delete',
-			aliases: ['delete'],
-			group: 'edit-meme',
-			memberName: 'delete',
-			description: 'delete someone',
+			name: 'trash',
+			aliases: ['SMtrash', 'trash', 'spidermantrash'],
+			group: 'edit-meme-2',
+			memberName: 'trash',
+			description: 'ur trash',
 			throttling: {
 				usages: 1,
 				duration: 10
@@ -26,13 +26,13 @@ module.exports = class DeleteCommand extends Command {
 					name: 'Wikimedia Commons',
 					url: 'https://commons.wikimedia.org/wiki/Main_Page',
 					reason: 'Image',
-					reasonURL: 'https://commons.wikimedia.org/wiki/File:delete_blank.svg'
+					reasonURL: 'https://commons.wikimedia.org/wiki/File:trash_blank.svg'
 				}
 			],
 			args: [
 				{
 					key: 'image',
-					prompt: 'who is getting deleted?',
+					prompt: 'who is the trash?',
 					type: 'image-or-avatar',
 					default: msg => msg.author.displayAvatarURL({ format: 'png', size: 512 })
 				}
@@ -41,8 +41,8 @@ module.exports = class DeleteCommand extends Command {
 	}
 
 	async run(msg, { image }) {
-        let img = await new DIG.Delete().getImage(image)
-		let attach = new Discord.MessageAttachment(img, "delete.png");;
+        let img = await new DIG.Trash().getImage(image)
+		let attach = new Discord.MessageAttachment(img, "trash.png");;
         return msg.channel.send(attach)
 	}
 };
