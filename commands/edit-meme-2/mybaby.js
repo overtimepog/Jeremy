56c21ed6dca6101bc79a395d67ac198bfd8fdc68
Overtime <Truen2005@gmail.com>
2021-05-11 13:50:35 -0400

    Create mybaby.js

---
+++ commands/edit-meme-2/mybaby.js
@@ -0,0 +1,34 @@
+const Command = require('../../structures/Command');
+const DIG = require("discord-image-generation");
+const Discord = require("discord.js")
+
+module.exports = class MyBabyCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'mybaby',
+			aliases: ['affect', 'affect-my-baby', 'mom-affect'],
+			group: 'edit-meme-2',
+			memberName: 'mybaby',
+			description: 'no it doesnt affect my baby',
+			throttling: {
+				usages: 1,
+				duration: 10
+			},
+			clientPermissions: ['ATTACH_FILES'],
+			args: [
+				{
+					key: 'image',
+					prompt: 'who is the affected child looking at?',
+					type: 'image-or-avatar',
+					default: msg => msg.author.displayAvatarURL({ format: 'png', size: 512 })
+				}
+			]
+		});
+	}
+
+	async run(msg, { image }) {
+        let img = await new DIG.Affect().getImage(image)
+		let attach = new Discord.MessageAttachment(img, "mybaby.png");;
+        return msg.channel.send(attach)
+	}
+};
