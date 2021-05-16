677cdffac5555427ec27ac52eba44bf3d6ca9dcb
Overtime <Truen2005@gmail.com>
2021-05-16 19:41:37 -0400

    made roblox and failure

---
+++ commands/edit-meme/failure.js
@@ -0,0 +1,34 @@
+const Command = require('../../structures/Command');
+const Meme = require("memer-api");
+const memer = new Meme();
+
+module.exports = class failureCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'failure',
+			group: 'edit-meme-2',
+			memberName: 'failure',
+			description: 'you\'re a failure',
+			throttling: {
+				usages: 1,
+				duration: 10
+			},
+			clientPermissions: ['ATTACH_FILES'],
+			args: [
+				{
+					key: 'image',
+					prompt: 'who is the failure?',
+					type: 'image-or-avatar',
+					default: msg => msg.author.displayAvatarURL({ format: 'png', size: 512 })
+				}
+			]
+		});
+	}
+
+	async run(msg, { image }) {
+        memer.failure(image).then(img => {
+      	const attachment = new Discord.MessageAttachment(img, "failure.png");
+      	msg.channel.send(attachment)
+		})
+	}
+};
