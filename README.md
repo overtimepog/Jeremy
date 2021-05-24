55a742780fba53fdfc02ede0418b16c62824ce5f
Overtime <Truen2005@gmail.com>
2021-05-24 16:42:08 -0400

    added byemom

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 # Jeremy
-Jeremy is a Discord bot coded in JavaScript with
-[discord.js](https://discord.js.org/) using the
-[Commando](https://github.com/discordjs/Commando) command framework. With over
-500 commands, it is one of the most feature-rich bots out there.
+Jeremy Racoon: Discords most extensive meme bot with over 180+ memes to create
---
+++ commands/edit-meme/byemom.js
@@ -0,0 +1,46 @@
+const Command = require('../../structures/Command');
+const DIG = require("discord-image-generation");
+const Discord = require("discord.js")
+
+module.exports = class ByeMomCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'byemom',
+			group: 'edit-meme',
+			memberName: 'byemom',
+			description: 'bye mom, cya',
+			throttling: {
+				usages: 1,
+				duration: 10
+			},
+			clientPermissions: ['ATTACH_FILES'],
+			args: [
+				{
+					key: 'avatar',
+					prompt: 'who is saying bye?',
+					type: 'image-or-avatar',
+					default: msg => msg.author.displayAvatarURL({ format: 'png', size: 512 })
+				},
+				{
+					key: 'username',
+					prompt: 'whats their name?',
+					type: 'string',
+					default: msg => msg.author.username
+				},
+				{
+					key: 'text',
+					prompt: 'what are they searching?',
+					type: 'string'
+				},
+
+			]
+		});
+	}
+
+	async run(msg, { avatar, username, text }) {
+		memer.byemom(avatar, username, text).then(image => {
+		const attachment = new Discord.MessageAttachment(image, "byemom.png");
+      	msg.channel.send(attachment)
+		})
+	}
+};
