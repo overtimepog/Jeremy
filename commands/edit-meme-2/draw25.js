5e2d5c6a79aaf0a7d2d3b01c087553886be20cd3
Overtime <Truen2005@gmail.com>
2021-05-25 03:49:28 -0400

    Update draw25.js

--- commands/edit-meme-2/draw25.js
+++ commands/edit-meme-2/draw25.js
@@ -60,7 +60,7 @@ module.exports = class Draw25Command extends Command {
 			ctx.textAlign = 'center';
 			ctx.fillStyle = "white"
 			ctx.font = '25px Noto';
-			ctx.fillText(card.join('\n'), 120, 165, 220);
+			ctx.fillText(card.join('\n'), 120, 168, 220);
         return msg.say({ files: [{ attachment: canvas.toBuffer(), name: 'draw25.png' }] });
     }
 };
