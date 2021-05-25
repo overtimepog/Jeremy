7872e72529740c13edbde94f64b6ddf198398e8a
Overtime <Truen2005@gmail.com>
2021-05-24 23:00:42 -0400

    Update youtube.js

--- commands/edit-meme-4/youtube.js
+++ commands/edit-meme-4/youtube.js
@@ -27,7 +27,7 @@ module.exports = class YoutubeCommand extends Command {
 					key: 'user',
 					prompt: 'Whats this persons youtube name?',
 					type: 'string',
-					default: msg => msg.author.username
+					default: msg => msg.member.user.username
 				},
 				{
 					key: 'text',
