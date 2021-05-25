ea2ce0d6c8d3de8b5ff7d038b20c26dedd79fce7
Overtime <Truen2005@gmail.com>
2021-05-24 20:45:36 -0400

    Update draw25.js

--- commands/edit-meme-2/draw25.js
+++ commands/edit-meme-2/draw25.js
@@ -33,7 +33,7 @@ module.exports = class Draw25Command extends Command {
 			args: [
 				{
 					key: 'avatar',
-					prompt: 'who is saying bye?',
+					prompt: 'who is drawing the cards?',
 					type: 'image-or-avatar',
 					default: msg => msg.author.displayAvatarURL({ format: 'png', size: 512 })
 				},
@@ -47,7 +47,7 @@ module.exports = class Draw25Command extends Command {
 		});
 	}
 
-    async run(msg, { cards, guy }) {
+    async run(msg, { cards, avatar }) {
         const base = await loadImage(path.join(__dirname, '..', '..', 'assets', 'images', 'draw25.png'));
         const canvas = createCanvas(base.width, base.height);
         const ctx = canvas.getContext('2d');
@@ -56,7 +56,7 @@ module.exports = class Draw25Command extends Command {
         const card = await wrapText(ctx, "​    " + (cards.trim()), 150);
         ctx.fillText(card.join('\n'), 85, 160, 120);
         ctx.textAlign = "center";
-		const { body } = await request.get(guy);
+		const { body } = await request.get(avatar);
 		const data = await loadImage(body);
 		const { x, y, width, height } = centerImagePart(data, 310, 65, 410, 112);
 		ctx.drawImage(data, x, y, width, height);
