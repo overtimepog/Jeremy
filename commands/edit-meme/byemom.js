ddf8c7567ab4dc58afebb027a06f0859d1ec3cf5
Overtime <Truen2005@gmail.com>
2021-05-24 22:59:44 -0400

    Update byemom.js

--- commands/edit-meme/byemom.js
+++ commands/edit-meme/byemom.js
@@ -26,7 +26,7 @@ module.exports = class ByeMomCommand extends Command {
 					key: 'username',
 					prompt: 'whats their name?',
 					type: 'string',
-					default: msg => msg.user.username
+					default: msg => msg.member.user.username
 				},
 				{
 					key: 'text',
